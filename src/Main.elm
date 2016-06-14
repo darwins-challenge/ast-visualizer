8b4d30f715817c13437dc3de4b900895b9bd706c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-06-14 20:27:14 +0200

    Create a beginnerProgram
    A beginnerProgam accepts a model, update and view, the basics of an Elm project

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,42 @@
 module Ast exposing (..)
 
-import Html exposing (text)
+import Html exposing (Html, text)
+import Html.App exposing (beginnerProgram)
+
 
 main =
-  text "Hello, World!"
+  beginnerProgram
+    { model = model
+    , update = update
+    , view = view
+    }
+
+
+-- MODEL
+
+
+type alias Model = String
+
+
+model : Model
+model = "Hello, World!"
+
+
+-- UPDATE
+
+
+type Message =
+  DoNothing
+
+update : Message -> Model -> Model
+update message model =
+  case message of
+    _ -> model
+
+
+-- VIEW
+
+
+view : Model -> Html Message
+view model =
+  text model
