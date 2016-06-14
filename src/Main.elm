0caac632411b0af9355c41ac8fdfcabe325f9405
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-06-14 20:46:28 +0200

    Describe the abstract syntax tree

--- src/Main.elm
+++ src/Main.elm
@@ -14,6 +14,48 @@ main =
 
 -- MODEL
 
+type Program =
+    If Condition Program Program
+  | Command Command
+
+
+type Condition =
+    True
+  | False
+  | Not Condition
+  | Or Condition Condition
+  | And Condition Condition
+  | Less Expression Expression
+  | LessEqual Expression Expression
+  | Equal Expression Expression
+  | GreaterEqual Expression Expression
+  | Greater Expression Expression
+
+
+type Expression =
+    Constant Float
+  | Sensor Sensor
+  | Plus Expression Expression
+  | Minus Expression Expression
+  | Multiply Expression Expression
+  | Divide Expression Expression
+
+type Sensor =
+    X
+  | Y
+  | Vx
+  | Vy
+  | O
+  | W
+  | Fuel
+
+
+type Command =
+    Skip
+  | Left
+  | Right
+  | Thrust
+
 
 type alias Model = String
 
