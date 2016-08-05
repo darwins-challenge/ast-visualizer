05cfb780342f4092c43c8eb05aef5678bbc81cf3
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-08-05 08:26:19 +0200

    Group the svg elements

--- src/Ast/Svg.elm
+++ src/Ast/Svg.elm
@@ -10,7 +10,9 @@ programAsSvg : Ast.Types.Program -> Html Message
 programAsSvg program =
   div [] [
     Svg.svg [ width "480", height "480" ] [
-       rect [ x "0", y "0", width "480", height "480" ] []
+       g [ fill "none", stroke "black" ] [
+          rect [ x "0", y "0", width "480", height "480" ] []
+       ]
     ]
   , Html.text (toString program)
   ]
