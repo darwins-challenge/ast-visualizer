4d4d1671d46d64d0aef5d5835a2a74febc6f833c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-08-05 08:24:07 +0200

    Create a Svg element

--- src/Ast/Svg.elm
+++ src/Ast/Svg.elm
@@ -1,9 +1,16 @@
 module Ast.Svg exposing (programAsSvg)
 
-import Html exposing (Html, text)
+import Html exposing (Html, text, div)
 import Ast.Types
 import Ast.Message exposing (Message)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 programAsSvg : Ast.Types.Program -> Html Message
 programAsSvg program =
-  text (toString program)
+  div [] [
+    Svg.svg [ width "480", height "480" ] [
+       rect [ x "0", y "0", width "480", height "480" ] []
+    ]
+  , Html.text (toString program)
+  ]
